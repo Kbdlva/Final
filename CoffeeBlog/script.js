84ab413ae77f5d5e1c3a3b52a2de16e231383437
akerke <210103371@stu.sdu.edu.kz>
2022-12-19 17:43:41 +0600

    update

--- CoffeeBlog/script.js
+++ CoffeeBlog/script.js
@@ -1,10 +1,3 @@
- $(document).ready(function(){
-    $('.header_burger').click(function(event){
-        $('.header_burger').toggleClass('active');
-        $('.navbar').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
 
 
 const btns = document.querySelectorAll(".nav-btn");
@@ -12,9 +5,10 @@ const slides = document.querySelectorAll(".video-slide");
 const contents = document.querySelectorAll(".content");
 const popups = document.querySelectorAll(".popup-screen");
 const opens = document.querySelectorAll(".open");
+const closes = document.querySelectorAll(".closeBtn");
 
 
-var sliderNav = function(manual){
+var sliderNav = function (manual) {
     btns.forEach((btn) => {
         btn.classList.remove("active");
     });
@@ -26,21 +20,37 @@ var sliderNav = function(manual){
     contents.forEach((content) => {
         content.classList.remove("active");
     });
-
+    opens[manual].addEventListener('click', () => {
+        popups[manual].classList.add('popup-screen-active');
+    })
+    closes[manual].addEventListener('click', () => {
+        popups[manual].classList.remove('popup-screen-active');
+    })
+    closes[manual]
     btns[manual].classList.add("active");
     slides[manual].classList.add("active");
     contents[manual].classList.add("active");
-    
+
 }
-
-
-
-
+sliderNav(0);
 btns.forEach((btn, i) => {
     btn.addEventListener("click", () => {
+        console.log(i);
         sliderNav(i);
     })
-})
+});
+
+$(document).ready(function () {
+    $('.header_burger').click(function (event) {
+        $('.header_burger').toggleClass('active');
+        $('.navbar').toggleClass('active');
+        $('body').toggleClass('lock');
+
+    });
+});
+
+
+
 
 
 
